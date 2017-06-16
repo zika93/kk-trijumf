b6e8127ec857c713e81508ac857bd4d53548547e
Nemanja <neca@svrlji.net>
2017-06-16 14:23:21 +0200

    images, navbar and err

---
+++ src/app/model/player.ts
@@ -0,0 +1,13 @@
+export class Player {
+
+  constructor(
+    private id: number,
+    private  name: string,
+    private surname: string,
+    private date: Date,
+    private height: number,
+    private weight: number,
+    private sportId: number) {
+  }
+
+}
--- src/app/player-list/player/player.component.ts
+++ src/app/player-list/player/player.component.ts
@@ -1,11 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
+import {Player} from '../../model/player';
+
 @Component({
   selector: 'app-player',
   templateUrl: './player.component.html',
   styleUrls: ['./player.component.css']
 })
 export class PlayerComponent implements OnInit {
+
+  player: Player;
 
   constructor() { }
 
